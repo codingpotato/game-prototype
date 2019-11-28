66b2f48d557aab0022efaa68818b7a29371a3927
codingpotato <codingpotato@gmail.com>
2019-11-28 22:30:54 +0800

    Board for color fill

--- color_fill/board.hpp
+++ color_fill/board.hpp
@@ -7,3 +7,15 @@ struct tile {
 };
 
 using board = matrix<tile>;
+
+template <typename T>
+inline std::pair<position, position> find_empty_neighber_tiles(
+    const matrix<T>& m) noexcept {
+  while (true) {
+    position pos{std::rand() % b.rows(), std::rand() % b.columns()};
+    if (m[pos] == T{}) {
+    }
+  }
+}
+
+inline bool fill_two_neighber_tiles(board& b, size_t color) noexcept {}
